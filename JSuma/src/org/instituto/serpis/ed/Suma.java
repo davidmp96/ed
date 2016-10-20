b66159772821f9d7bc1d44c3fb83fbb9a64db6e6
davidmp96 <deivid13296@gmail.com>
2016-10-20 12:39:51 +0200

    Añadido JSuma

---
+++ JSuma/src/org/instituto/serpis/ed/Suma.java
@@ -0,0 +1,22 @@
+package org.instituto.serpis.ed;
+
+import java.math.BigDecimal;
+import java.util.Scanner; 
+
+public class Suma {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Programa Suma");
+		System.out.print(  "Introduce el primer número: ");
+		String primerNumero = scanner.nextLine();
+		BigDecimal primero = new BigDecimal(primerNumero);
+		System.out.print( "Introduce el segundo número: ");
+		String segundoNumero = scanner.nextLine();
+		BigDecimal segundo = new BigDecimal(segundoNumero);
+		BigDecimal suma = BigDecimal.ZERO;
+		suma = suma.add(primero).add(segundo);
+		System.out.println(                 "La suma es: " + suma);
+	}
+
+}
