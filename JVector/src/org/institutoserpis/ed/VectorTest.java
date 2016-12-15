16d5482ed2bd38ce54c9853193a2cc4cd81884a2
davidmp96 <deivid13296@gmail.com>
2016-12-15 12:50:00 +0100

    JVector añadido min (con tests unitarios)

--- JVector/src/org/institutoserpis/ed/VectorTest.java
+++ JVector/src/org/institutoserpis/ed/VectorTest.java
@@ -19,4 +19,11 @@ public class VectorTest {
 	public void indexOfEmpty(){
 		assertEquals(-1, Vector.indexOf(new int[]{}, 123));
 	}
+	
+	@Test
+	public void min(){
+		assertEquals(3, Vector.min(new int[]{3, 15, 7, 9, 12}));
+		assertEquals(7, Vector.min(new int[]{32, 15, 7, 9, 12}));
+		assertEquals(2, Vector.min(new int[]{32, 15, 7, 9, 2}));
+	}
 }
