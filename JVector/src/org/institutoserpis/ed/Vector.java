fe6680b8605be491b57b4a713004456c3eb43903
davidmp96 <deivid13296@gmail.com>
2017-01-19 11:42:09 +0100

    JVector modificado con Test

--- JVector/src/org/institutoserpis/ed/Vector.java
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -35,6 +35,39 @@ public class Vector {
 				min = item;
 		return min;
 	}
+	
+	public static void selectionSortUgly(int[] v) {
+		for (int itemIndex = 0; itemIndex < v.length - 1; itemIndex++){
+			int indexOfMin = itemIndex;
+			for(int index = itemIndex + 1; index < v.length; index++)
+				if (v[index] < v[indexOfMin])
+					indexOfMin = index;
+			int aux = v[itemIndex];
+			v[itemIndex] = v[indexOfMin];
+			v[indexOfMin] = aux;
+		}
+	}
+	
+	private static int indexOfMin(int[] v, int initialIndex) {
+		int indexOfMin = initialIndex;
+		for(int index = initialIndex + 1; index < v.length; index++)
+			if (v[index] < v[indexOfMin])
+				indexOfMin = index;
+		return indexOfMin;
+	}
+	
+	private static void swap(int[] v, int indexOne, int indexTwo){
+		int aux = v[indexOne];
+		v[indexOne] = v[indexTwo];
+		v[indexTwo] = aux;
+	}
+	
+	public static void selectionSort(int[] v) {
+		for (int itemIndex = 0; itemIndex < v.length - 1; itemIndex++){
+			int indexOfMin = indexOfMin(v, itemIndex);
+			swap(v, itemIndex, indexOfMin);
+		}
+	}
 }
 
 
--- JVector/src/org/institutoserpis/ed/VectorTest.java
+++ JVector/src/org/institutoserpis/ed/VectorTest.java
@@ -6,24 +6,39 @@ import org.junit.Test;
 
 public class VectorTest {
 
-	@Test 
+	@Test
 	public void indexOf() {
-		assertEquals(0, Vector.indexOf(new int[]{32, 15, 7, 9, 12}, 32));
-		assertEquals(2, Vector.indexOf(new int[]{32, 15, 7, 9, 12}, 7));
-		assertEquals(3, Vector.indexOf(new int[]{32, 15, 7, 9, 12}, 9));
-		assertEquals(4, Vector.indexOf(new int[]{32, 15, 7, 9, 12}, 12));
-		assertEquals(-1, Vector.indexOf(new int[]{32, 15, 7, 9, 12}, 99));
+		assertEquals(0, Vector.indexOf(new int[] { 32, 15, 7, 9, 12 }, 32));
+		assertEquals(2, Vector.indexOf(new int[] { 32, 15, 7, 9, 12 }, 7));
+		assertEquals(3, Vector.indexOf(new int[] { 32, 15, 7, 9, 12 }, 9));
+		assertEquals(4, Vector.indexOf(new int[] { 32, 15, 7, 9, 12 }, 12));
+		assertEquals(-1, Vector.indexOf(new int[] { 32, 15, 7, 9, 12 }, 99));
 	}
-	
+
 	@Test
-	public void indexOfEmpty(){
-		assertEquals(-1, Vector.indexOf(new int[]{}, 123));
+	public void indexOfEmpty() {
+		assertEquals(-1, Vector.indexOf(new int[] {}, 123));
 	}
-	
+
 	@Test
-	public void min(){
-		assertEquals(3, Vector.min(new int[]{3, 15, 7, 9, 12}));
-		assertEquals(7, Vector.min(new int[]{32, 15, 7, 9, 12}));
-		assertEquals(2, Vector.min(new int[]{32, 15, 7, 9, 2}));
+	public void min() {
+		assertEquals(3, Vector.min(new int[] { 3, 15, 7, 9, 12 }));
+		assertEquals(7, Vector.min(new int[] { 32, 15, 7, 9, 12 }));
+		assertEquals(2, Vector.min(new int[] { 32, 15, 7, 9, 2 }));
+	}
+
+	@Test
+	public void selectionSort() {
+		int[] v = new int[] { 32, 15, 7, 9, 12 };
+		Vector.selectionSort(v);
+		assertArrayEquals(new int[] { 7, 9, 12, 15, 32 }, v);
+
+		v = new int[] { 32, 7, 15, 9, 12 };
+		Vector.selectionSort(v);
+		assertArrayEquals(new int[] { 7, 9, 12, 15, 32 }, v);
+		
+		v = new int[] { 32, 15, 12, 9, 7 };
+		Vector.selectionSort(v);
+		assertArrayEquals(new int[] { 7, 9, 12, 15, 32 }, v);
 	}
 }
